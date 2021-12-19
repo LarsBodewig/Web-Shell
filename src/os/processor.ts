2d095f51fbd0a187968712b63861a8d6a416617f
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2021-12-19 14:11:47 +0100

    Added echo for tests

--- src/os/processor.ts
+++ src/os/processor.ts
@@ -1,6 +1,6 @@
 const CLOCK_SPEED_MS = 100;
 
-export async function waitFor(condition: () => boolean) {
+export async function waitFor(condition: () => boolean): Promise<void> {
   while (!condition()) {
     window.setTimeout(() => {}, CLOCK_SPEED_MS);
   }
