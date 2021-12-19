765e622b5838b71bcfe29b7e89262a1d52dd4beb
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2021-12-19 14:11:47 +0100

    Added echo for tests

--- src/os/command.ts
+++ src/os/command.ts
@@ -1,25 +1,19 @@
 import { InputStream, OutputStream } from "./stream";
 
+export type StreamFunction<I = void, O = void> = (a: I) => O;
+
 export abstract class Command<I, A extends Arguments, O> {
-  public in: InputStream<I>;
-  public out: OutputStream<O>;
-
-  constructor(input = InputStream.void, output = OutputStream.void) {
-    this.in = input;
-    this.out = output;
+  public call(args: A): StreamFunction<InputStream<I>, OutputStream<O>> {
+    return (input: InputStream<I>) => {
+      const output = this.process(input, args);
+      output.close();
+      return output;
+    };
   }
 
-  public async execute(args: A) {
-    const input = await this.in.read();
-    const output = this.call(input, args);
-    this.out.write(output);
-  }
-
-  public abstract call(input: I, args: A): O;
+  protected abstract process(input: InputStream<I>, args: A): OutputStream<O>;
 }
 
 export type Arguments = {
   [n: string]: any;
 };
-
-// export class Value {}
--- src/os/operator.ts
+++
@@ -1,7 +0,0 @@
-import { Command } from "./command";
-import { InputStream, Stream } from "./stream";
-
-export function pipe<T>(from: Command<any, any, T>, to: Command<T, any, any>) {
-  // does this work?
-  to.in = from.out as Stream<T> as InputStream<T>;
-}
