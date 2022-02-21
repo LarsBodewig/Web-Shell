25b4c52673f3a1e2d6c8ac5f6f47c02516bde37e
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2022-02-21 18:48:41 +0100

    Added input parser

--- src/action/input.ts
+++ src/action/input.ts
@@ -1,10 +1,5 @@
-import clone from "just-clone";
 import { addReducer } from "../redux/store";
 
-export const INPUT = addReducer<string>((state, action) => {
-  const input = action.payload;
-  state.prevState = clone(state);
-  state.prevState.prompt.result = "input was: " + input; // await parse(input);
-  state.id = state.id + 1;
-  state.prompt.input = "";
+export const INPUT = addReducer((state, _action) => {
+  state.locked = true;
 });
