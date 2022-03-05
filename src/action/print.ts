c8d35dcc3a02c3ba3523e4d264db8474e1b27c92
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2022-03-05 23:32:26 +0100

    Fixed linebreaks in result

--- src/action/print.ts
+++ src/action/print.ts
@@ -1,6 +1,10 @@
 import { addReducer } from "../redux/store";
 
 export const PRINT = addReducer<string>((state, action) => {
-  const result = action.payload;
-  state.prompt.result = (state.prompt.result ?? "") + result;
+  const value = action.payload;
+  if (state.prompt.result === undefined) {
+    state.prompt.result = value;
+  } else {
+    state.prompt.result = state.prompt.result + "<br>" + value;
+  }
 });
