3c3101e856bcf7988695d6189679efcaa1da0387
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2022-03-06 19:14:27 +0100

    Added pwd command

---
+++ src/bin/pwd.ts
@@ -0,0 +1,25 @@
+import { Command } from "../os/command";
+import { InputStreamVoid, OutputStream } from "../os/stream";
+import { locToUrl } from "../redux/location";
+import { store } from "../redux/store";
+
+export class Pwd extends Command<void, {}, string> {
+  constructor() {
+    super("pwd");
+  }
+
+  protected parseArgs(_args?: string): {} {
+    return {};
+  }
+
+  protected process(
+    _input: InputStreamVoid,
+    _args: {},
+    output: OutputStream<string>
+  ): Promise<OutputStream<string>> {
+    const state = store.getState();
+    const path = locToUrl(state.location);
+    output.write(path);
+    return Promise.resolve(output);
+  }
+}
--- src/os/path.ts
+++ src/os/path.ts
@@ -1,7 +1,8 @@
 import { Echo } from "../bin/echo";
+import { Pwd } from "../bin/pwd";
 import { Command } from "./command";
 
-export const path = [new Echo()];
+export const path = [new Echo(), new Pwd()];
 
 export function findInPath(name: string): Command<any, any, any> | undefined {
   return path.find((cmd) => cmd.name === name);
