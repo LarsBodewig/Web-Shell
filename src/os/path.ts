4d2e1cfec15ef19b3a972044fb97f9e1fdd9fbe7
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2022-03-07 19:16:48 +0100

    Added basic tail command

--- src/os/path.ts
+++ src/os/path.ts
@@ -1,8 +1,9 @@
 import { Echo } from "../bin/echo";
 import { Pwd } from "../bin/pwd";
+import { Tail } from "../bin/tail";
 import { Command } from "./command";
 
-export const path = [new Echo(), new Pwd()];
+export const path = [new Echo(), new Pwd(), new Tail()];
 
 export function findInPath(name: string): Command<any, any, any> | undefined {
   return path.find((cmd) => cmd.name === name);
